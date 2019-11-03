d9cd999d165fbb1be3b7bfe03b85da7c25aee24c
Sucharu Rai <raisucharugzp@gmail.com>
2019-11-03 13:00:42 +0530

    REbased (#1)
    * Tab responsiveness added plus some changes
    
    * Added studio page
    
    * Added studio page
    
    * Bugs fixed
    
    * Cards Separate Page added
    
    * Dev (#7)
    
    * Added roles page
    
    * Responsive fix
    
    * Page linking
    
    * Linked ContactUs
    
    * added navbar
    
    * Added js fix
    
    * Video Changed
    
    * Added content
    
    * Added google analytics
    
    * Changed the mobile css
    
    * Responsiveness Rectified
    
    * Create README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+#### This is just a sosrgstudios raw web page code
+#### For organised code page, visit "sosrgstudios" github repo
--- main.js
+++ main.js
@@ -1,75 +1,108 @@
 (function($, win) {
-  $.fn.inViewport = function(cb) {
-     return this.each(function(i,el) {
-       function visPx(){
-         var elH = $(el).outerHeight(),
-             H = $(win).height(),
-             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
-         return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));  
-       }
-       visPx();
-       $(win).on("resize scroll", visPx);
-     });
-  };
+    $.fn.inViewport = function(cb) {
+        return this.each(function(i, el) {
+            function visPx() {
+                var elH = $(el).outerHeight(),
+                    H = $(win).height(),
+                    r = el.getBoundingClientRect(),
+                    t = r.top,
+                    b = r.bottom;
+                return cb.call(el, Math.max(0, t > 0 ? Math.min(elH, H - t) : Math.min(b, H)));
+            }
+            visPx();
+            $(win).on("resize scroll", visPx);
+        });
+    };
 }(jQuery, window));
 
 $('#feature').inViewport(function(px) {
-  if(px > 0) {
-    
-  } else {
+    if (px > 0) {
 
-  }
+    } else {
+
+    }
 })
 
-$("#feature .content-left").inViewport(function(px) {
-  if(px > 0) {
-    $(this).addClass("leftEnter");
-  }
-  else{
-    $(this).removeClass("leftEnter");
-  }
-});
-$("#feature img").inViewport(function(px) {
-  if(px > 0) {
-    $(this).addClass("rightEnter");
-  }
-  else{
-    $(this).removeClass("rightEnter");
-  }
-});
+$('#hero').inViewport(function(px) {
+    if (px > 0) {
+        $("nav").hide()
+    } else {
+        $("nav").show()
+    }
+})
+
+// $("#feature .content-left").inViewport(function(px) {
+//   if(px > 0) {
+//     $(this).addClass("leftEnter");
+//   }
+//   else{
+//     $(this).removeClass("leftEnter");
+//   }
+// });
+// $("#feature img").inViewport(function(px) {
+//   if(px > 0) {
+//     $(this).addClass("rightEnter");
+//   }
+//   else{
+//     $(this).removeClass("rightEnter");
+//   }
+// });
 
 $(".word1").inViewport(function(px) {
-  if(px > 0) {
-    $(this).addClass("slidein");
-  }
-  else{
-    $(this).removeClass("slidein");
-  }
+    if (px > 0) {
+        $(this).addClass("slidein");
+    } else {
+        $(this).removeClass("slidein");
+    }
 });
 
 $(".word2").inViewport(function(px) {
-  if(px > 0) {
-    $(this).addClass("slideout");
-  }
-  else{
-    $(this).removeClass("slideout");
-  }
+    if (px > 0) {
+        $(this).addClass("slideout");
+    } else {
+        $(this).removeClass("slideout");
+    }
 });
-$(".content").inViewport(function(px) {
-  if(px > 0) {
-    $(this).addClass("revealText");
-  }
-  else{
-    $(this).removeClass("revealText");
-  }
+$("#hero .content").inViewport(function(px) {
+    if (px > 0) {
+        $(this).addClass("revealText");
+    } else {
+        $(this).removeClass("revealText");
+    }
+});
+$("#hero-section .content").inViewport(function(px) {
+    if (px > 0) {
+        $(this).addClass("revealText");
+    } else {
+        $(this).removeClass("revealText");
+    }
 });
 
 $(".service").inViewport(function(px) {
-  if(px > 0) {
-    $(this).removeClass("opacityExit");
-    $(this).addClass("opacityEnter");
-  } else {
-    $(this).removeClass("opacityEnter");
-    $(this).addClass("opacityExit");
-  }
+    if (px > 0) {
+        $(this).removeClass("opacityExit");
+        $(this).addClass("opacityEnter");
+    } else {
+        $(this).removeClass("opacityEnter");
+        $(this).addClass("opacityExit");
+    }
+})
+$(".feature-section").inViewport(function(px) {
+    if (px > 0) {
+        $(this).removeClass("opacityExit");
+        $(this).addClass("opacityEnter");
+    } else {
+        $(this).removeClass("opacityEnter");
+        $(this).addClass("opacityExit");
+    }
+})
+
+$("#work").inViewport(function(px) {
+    if (px > 0) {
+        $(this).removeClass("opacityExit");
+        $(this).addClass("opacityEnter");
+    } else {
+        $(this).removeClass("opacityEnter");
+        $(this).addClass("opacityExit");
+    }
 })
