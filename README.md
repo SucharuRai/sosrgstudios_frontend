ab4b362cc51535b51797760c55cf774de6bc1fd9
Sucharu Rai <raisucharugzp@gmail.com>
2019-11-03 13:03:28 +0530

    Revert "REbased (#1)"
    This reverts commit d9cd999d165fbb1be3b7bfe03b85da7c25aee24c.

--- README.md
+++
@@ -1,2 +0,0 @@
-#### This is just a sosrgstudios raw web page code
-#### For organised code page, visit "sosrgstudios" github repo
--- main.js
+++ main.js
@@ -1,108 +1,75 @@
 (function($, win) {
-    $.fn.inViewport = function(cb) {
-        return this.each(function(i, el) {
-            function visPx() {
-                var elH = $(el).outerHeight(),
-                    H = $(win).height(),
-                    r = el.getBoundingClientRect(),
-                    t = r.top,
-                    b = r.bottom;
-                return cb.call(el, Math.max(0, t > 0 ? Math.min(elH, H - t) : Math.min(b, H)));
-            }
-            visPx();
-            $(win).on("resize scroll", visPx);
-        });
-    };
+  $.fn.inViewport = function(cb) {
+     return this.each(function(i,el) {
+       function visPx(){
+         var elH = $(el).outerHeight(),
+             H = $(win).height(),
+             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+         return cb.call(el, Math.max(0, t>0? Math.min(elH, H-t) : Math.min(b, H)));  
+       }
+       visPx();
+       $(win).on("resize scroll", visPx);
+     });
+  };
 }(jQuery, window));
 
 $('#feature').inViewport(function(px) {
-    if (px > 0) {
+  if(px > 0) {
+    
+  } else {
 
-    } else {
-
-    }
+  }
 })
 
-$('#hero').inViewport(function(px) {
-    if (px > 0) {
-        $("nav").hide()
-    } else {
-        $("nav").show()
-    }
-})
-
-// $("#feature .content-left").inViewport(function(px) {
-//   if(px > 0) {
-//     $(this).addClass("leftEnter");
-//   }
-//   else{
-//     $(this).removeClass("leftEnter");
-//   }
-// });
-// $("#feature img").inViewport(function(px) {
-//   if(px > 0) {
-//     $(this).addClass("rightEnter");
-//   }
-//   else{
-//     $(this).removeClass("rightEnter");
-//   }
-// });
+$("#feature .content-left").inViewport(function(px) {
+  if(px > 0) {
+    $(this).addClass("leftEnter");
+  }
+  else{
+    $(this).removeClass("leftEnter");
+  }
+});
+$("#feature img").inViewport(function(px) {
+  if(px > 0) {
+    $(this).addClass("rightEnter");
+  }
+  else{
+    $(this).removeClass("rightEnter");
+  }
+});
 
 $(".word1").inViewport(function(px) {
-    if (px > 0) {
-        $(this).addClass("slidein");
-    } else {
-        $(this).removeClass("slidein");
-    }
+  if(px > 0) {
+    $(this).addClass("slidein");
+  }
+  else{
+    $(this).removeClass("slidein");
+  }
 });
 
 $(".word2").inViewport(function(px) {
-    if (px > 0) {
-        $(this).addClass("slideout");
-    } else {
-        $(this).removeClass("slideout");
-    }
+  if(px > 0) {
+    $(this).addClass("slideout");
+  }
+  else{
+    $(this).removeClass("slideout");
+  }
 });
-$("#hero .content").inViewport(function(px) {
-    if (px > 0) {
-        $(this).addClass("revealText");
-    } else {
-        $(this).removeClass("revealText");
-    }
-});
-$("#hero-section .content").inViewport(function(px) {
-    if (px > 0) {
-        $(this).addClass("revealText");
-    } else {
-        $(this).removeClass("revealText");
-    }
+$(".content").inViewport(function(px) {
+  if(px > 0) {
+    $(this).addClass("revealText");
+  }
+  else{
+    $(this).removeClass("revealText");
+  }
 });
 
 $(".service").inViewport(function(px) {
-    if (px > 0) {
-        $(this).removeClass("opacityExit");
-        $(this).addClass("opacityEnter");
-    } else {
-        $(this).removeClass("opacityEnter");
-        $(this).addClass("opacityExit");
-    }
-})
-$(".feature-section").inViewport(function(px) {
-    if (px > 0) {
-        $(this).removeClass("opacityExit");
-        $(this).addClass("opacityEnter");
-    } else {
-        $(this).removeClass("opacityEnter");
-        $(this).addClass("opacityExit");
-    }
-})
-
-$("#work").inViewport(function(px) {
-    if (px > 0) {
-        $(this).removeClass("opacityExit");
-        $(this).addClass("opacityEnter");
-    } else {
-        $(this).removeClass("opacityEnter");
-        $(this).addClass("opacityExit");
-    }
+  if(px > 0) {
+    $(this).removeClass("opacityExit");
+    $(this).addClass("opacityEnter");
+  } else {
+    $(this).removeClass("opacityEnter");
+    $(this).addClass("opacityExit");
+  }
 })
